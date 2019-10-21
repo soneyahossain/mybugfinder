51eed1918b59020af25edf06900d890ca67265fc
soneya <sh7hv@virginia.edu>
2019-10-20 22:46:03 -0400

    Changed code to visit ast

--- src/main/java/mytool.java
+++ src/main/java/mytool.java
@@ -2,14 +2,16 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.expr.BinaryExpr;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.UnaryExpr;
-import com.github.javaparser.ast.stmt.*;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.stmt.ThrowStmt;
+import com.github.javaparser.ast.stmt.TryStmt;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.printer.YamlPrinter;
 import com.github.javaparser.resolution.declarations.ResolvedFieldDeclaration;
 import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
@@ -40,6 +42,7 @@ public class mytool {
                 ParseResult<CompilationUnit> agendaCu = new JavaParser().parse(new FileInputStream(f));
                 CompilationUnit cu = agendaCu.getResult().get();
 
+
                 YamlPrinter printer = new YamlPrinter(true);
                 //System.out.println(printer.output(cu));
                 NodeList<TypeDeclaration<?>> types = cu.getTypes();
@@ -53,6 +56,7 @@ public class mytool {
                         ClassOrInterfaceDeclaration classC = cu.getClassByName(type.getName().asString()).get();
                         ResolvedReferenceTypeDeclaration typeDeclaration = JavaParserFacade.get(new ReflectionTypeSolver()).getTypeDeclaration(classC);
 
+
                         List<ResolvedFieldDeclaration> lists = typeDeclaration.getAllFields();
 
                         for (int i = 0; i < lists.size(); i++) {
@@ -60,119 +64,126 @@ public class mytool {
                             identifiers.put(lists.get(i).getName(), typeDeclaration.getField(lists.get(i).getName()).getType().describe());
                         }
                     }
-
                     //System.out.println(identifiers.toString());
-                    List<BodyDeclaration> members = type.getMembers();
-
-                    for (BodyDeclaration member : members) {
-
-                        if (member instanceof MethodDeclaration) {
-
-                            MethodDeclaration method = (MethodDeclaration) member;  //get the method
-                            NodeList<Statement> stmsts = method.getBody().get().getStatements();  //get all statements
-
-                            for (int i = 0; i < stmsts.size(); i++) {   // iterate all the statements
+                }
 
 
-                                Statement currentStmt = stmsts.get(i);
+                try {
+                    new VoidVisitorAdapter<Object>() {
+                        @Override
+                        public void visit(final BinaryExpr n, final Object arg) {
 
-                                if (currentStmt instanceof ReturnStmt) {   // show error in return statement
 
-                                    ReturnStmt rs = (ReturnStmt) currentStmt;
-                                    Expression ex = rs.getExpression().get();
+                            if (n.getOperator().toString().equals("NOT_EQUALS") || n.getOperator().toString().equals("EQUALS")) {
 
-                                    if (ex instanceof UnaryExpr && ((UnaryExpr) ex).getOperator().toString().equals("POSTFIX_INCREMENT")) {
+                                //System.out.println(n.toString());
+                                //System.out.println(n.getLeft());
+                                //System.out.println(n.getRight());
+                                //System.out.println(n.getOperator());
+
+
+                                if ((n.getLeft().isStringLiteralExpr() || n.getRight().isStringLiteralExpr())) {
+
+
+                                    System.out.print("Error found at File: " + f.getName());
+                                    System.out.print(", position: " + n.getBegin().get().toString());
+                                    System.out.print(", statement: " + n.toString());
+                                    System.out.println(", desc: " + "Illegal String Comparison");
+
+                                } else if (n.getLeft().isNameExpr()) {
+
+                                    String left = n.getLeft().toString();
+
+                                    if (identifiers.get(left) != null && identifiers.get(left).equals("java.lang.String")) {
+
 
                                         System.out.print("Error found at File: " + f.getName());
-                                        System.out.print(", position: " + rs.getBegin().get().toString());
-                                        System.out.print(", statement: " + rs.toString());
-                                        System.out.println(", Desc: This statement has a return such as return x++;. A postfix increment/decrement does not impact the value of the expression, so this increment/decrement has no effect. Please verify that this statement does the right thing\n");
-                                    }
-                                }
-
-
-                                if (currentStmt instanceof IfStmt) {
-
-
-                                    IfStmt binop = (IfStmt) currentStmt;
-                                    Expression exp = binop.getCondition();
-
-                                    if (exp instanceof BinaryExpr) {
-                                        // System.out.println( ((BinaryExpr) exp).getLeft());
-                                        // System.out.println( ((BinaryExpr) exp).getRight());
-                                        // System.out.println( ((BinaryExpr) exp).getOperator());
-                                        if (((BinaryExpr) exp).getLeft().isStringLiteralExpr() || ((BinaryExpr) exp).getRight().isStringLiteralExpr()) {
-
-
-                                            System.out.print("Error found at File: " + f.getName());
-                                            System.out.print(", position: " + binop.getBegin().get().toString());
-                                            System.out.print(", statement: " + exp.toString());
-                                            System.out.println(", desc: " + "Illegal String Comparison");
-
-                                        } else if (((BinaryExpr) exp).getLeft().isNameExpr()) {
-
-                                            String left = ((BinaryExpr) exp).getLeft().toString();
-
-                                            if (identifiers.get(left) != null && identifiers.get(left).equals("java.lang.String")) {
-
-
-                                                System.out.print("Error found at File: " + f.getName());
-                                                System.out.print(", position: " + binop.getBegin().get().toString());
-                                                System.out.print(", statement: " + exp.toString());
-                                                System.out.println(", desc: " + "Illegal String Comparison");
-                                            }
-
-                                        } else if (((BinaryExpr) exp).getRight().isNameExpr()) {
-
-                                            String right = ((BinaryExpr) exp).getLeft().toString();
-                                            if (identifiers.get(right) != null && identifiers.get(right).equals("java.lang.String")) {
-                                                System.out.print("Error found at File: " + f.getName());
-                                                System.out.print(", position: " + binop.getBegin().get().toString());
-                                                System.out.print(", statement: " + exp.toString());
-                                                System.out.println(", desc: " + "Illegal String Comparison");
-                                            }
-                                        }
+                                        System.out.print(", position: " + n.getBegin().get().toString());
+                                        System.out.print(", statement: " + n.toString());
+                                        System.out.println(", desc: " + "Illegal String Comparison");
                                     }
 
-                                }
+                                } else if (n.getRight().isNameExpr()) {
 
-                                if (currentStmt instanceof TryStmt) {
-
-                                    NodeList<Statement> stmsts1 = ((TryStmt) currentStmt).getTryBlock().getStatements();
-                                    stmsts.addAll(stmsts1);
-                                    stmsts1 = ((TryStmt) currentStmt).getFinallyBlock().get().getStatements();
-                                    stmsts.addAll(stmsts1);
-                                    ((TryStmt) currentStmt).getCatchClauses().forEach(catchClause -> {
-                                        stmsts.addAll(catchClause.getBody().getStatements());
-                                    });
-
-
-                                    // now handle finally blocks
-
-                                    NodeList<Statement> fbstmts = ((TryStmt) currentStmt).getFinallyBlock().get().getStatements();
-
-                                    fbstmts.forEach(statement -> {
-
-                                                if (statement instanceof ThrowStmt) {
-                                                    System.out.print("Error found at File: " + f.getName());
-                                                    System.out.print(", position: " + statement.getBegin().get().toString());
-                                                    System.out.print(", statement: " + statement.toString());
-                                                    System.out.println(", desc: " + "Finally block contains a throw statement");
-                                                }
-
-                                            }
-                                    );
+                                    String right = n.getLeft().toString();
+                                    if (identifiers.get(right) != null && identifiers.get(right).equals("java.lang.String")) {
+                                        System.out.print("Error found at File: " + f.getName());
+                                        System.out.print(", position: " + n.getBegin().get().toString());
+                                        System.out.print(", statement: " + n.toString());
+                                        System.out.println(", desc: " + "Illegal String Comparison");
+                                    }
                                 }
 
                             }
-                        }
-                    }
 
+                            n.getLeft().accept(this, arg);
+                            n.getRight().accept(this, arg);
+                            n.getComment().ifPresent((l) -> {
+                                l.accept(this, arg);
+                            });
+                        }
+
+                        @Override
+                        public void visit(final TryStmt n, final Object arg) {
+                            n.getCatchClauses().forEach((p) -> {
+                                p.accept(this, arg);
+                            });
+                            n.getFinallyBlock().ifPresent((l) -> {
+
+
+                                NodeList<Statement> fbstmts = n.getFinallyBlock().get().getStatements();
+
+                                fbstmts.forEach(statement -> {
+
+                                            if (statement instanceof ThrowStmt) {
+                                                System.out.print("Error found at File: " + f.getName());
+                                                System.out.print(", position: " + statement.getBegin().get().toString());
+                                                System.out.print(", statement: " + statement.toString());
+                                                System.out.println(", desc: " + "Finally block contains a throw statement");
+                                            }
+                                        }
+                                );
+
+
+                                l.accept(this, arg);
+                            });
+                            n.getResources().forEach((p) -> {
+                                p.accept(this, arg);
+                            });
+                            n.getTryBlock().accept(this, arg);
+                            n.getComment().ifPresent((l) -> {
+                                l.accept(this, arg);
+                            });
+                        }
+
+                        @Override
+                        public void visit(final ReturnStmt n, final Object arg) {
+                            n.getExpression().ifPresent((l) -> {
+
+                                ReturnStmt rs = n;
+                                Expression ex = rs.getExpression().get();
+
+                                if (ex instanceof UnaryExpr && ((UnaryExpr) ex).getOperator().toString().equals("POSTFIX_INCREMENT")) {
+
+                                    System.out.print("Error found at File: " + f.getName());
+                                    System.out.print(", position: " + rs.getBegin().get().toString());
+                                    System.out.print(", statement: " + rs.toString());
+                                    System.out.println(", Desc: This statement has a return such as return x++;. A postfix increment/decrement does not impact the value of the expression, so this increment/decrement has no effect. Please verify that this statement does the right thing\n");
+                                }
+                                l.accept(this, arg);
+                            });
+                            n.getComment().ifPresent((l) -> {
+                                l.accept(this, arg);
+                            });
+                        }
+                    }.visit(cu, null);
+                } catch (Exception e) {
+
+                    new RuntimeException(e);
                 }
             }
-
         } catch (Exception e) {
-            System.out.println(e.toString());
+            e.printStackTrace();
         }
     }
 }
